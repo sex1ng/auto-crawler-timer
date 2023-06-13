d35b83cb6003abf9f06e80c0e372a675c091c3fd
zhengsx <zhengshixin@zhonglian.com>
2023-06-13 16:08:47 +0800

    更新公共方法

--- common/generic.py
+++ common/generic.py
@@ -3,14 +3,14 @@ import datetime
 from typing import Text
 
 
-def root_path():
-    """ 获取 根路径 """
+# 获取项目根路径
+def root_path() -> str:
     path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return path
 
 
-def ensure_path_sep(path: Text) -> Text:
-    """ 兼容 windows 和 linux 不同环境的操作系统路径 """
+# 兼容操作系统路径分隔符
+def ensure_path_sep(path: str) -> str:
     if "/" in path:
         path = os.sep.join(path.split("/"))
 
@@ -20,13 +20,13 @@ def ensure_path_sep(path: Text) -> Text:
     return root_path() + path
 
 
-def save_crawler_result(content):
-    """ 写入文件 """
-    resources_path = "/data/python/resources/" + datetime.datetime.now().strftime('%Y-%m-%d')
-    resources_file = datetime.datetime.now().strftime('%H-%i-%s') + ".html"
-    if not os.path.isdir(resources_path):
-        os.mkdir(resources_path)
+# 保存爬虫结果
+def save_crawler_result(content: str) -> None:
+    path = root_path() + "/file/" + datetime.datetime.now().strftime('%Y-%m-%d')
+    document = datetime.datetime.now().strftime('%H:%M:%S') + ".html"
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-    with open(resources_path + "/" + resources_file, "w", encoding = 'utf-8') as f:
+    with open(path + "/" + document, "w", encoding = 'utf-8') as f:
         f.write(str(content))
         f.close()
