5b9515809ea2bbd0bc2ad70a8ad9fb6c947f5174
zhengsx <zhengshixin@zhonglian.com>
2023-06-20 15:09:29 +0800

    修改爬虫

--- jobs/crawler-beauty-timer.py
+++ jobs/crawler-beauty-timer.py
@@ -24,12 +24,11 @@ options.add_argument('--no-sandbox')  # 禁用沙盒模式(解决DevToolsActiveP
 options.add_argument('--headless')  # 无界面模式。
 options.add_argument('--disable-gpu')  # 禁用 GPU 加速。
 options.add_argument('--disable-extensions')  # 禁用扩展。
-options.add_argument('window-size=1920x1080')  # 设置窗口大小。
+options.add_argument('--window-size=1920x1080')  # 设置窗口大小。
 options.add_argument('--disable-dev-shm-usage')  # 禁用 /dev/shm。
 options.add_argument('--disable-blink-features=AutomationControlled')  # 禁用自动化。
-options.add_argument('--disable-blink-features=AutomationControlled')  # 去除 webdriver 痕迹。
 options.add_argument('--disable-blink-features=BlockCredentialedSubresources')  # 禁用阻止凭据资源的功能。
-options.add_experimental_option("excludeSwitches", ["enable-automation"])  # 禁用自动化扩展插件。
+options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 禁用自动化扩展插件。
 options.add_experimental_option('useAutomationExtension', False)  # 隐藏被浏览器被自动化工具控制横幅。
 
 UserAgent = random.choice(user_agent_tuple)
@@ -45,24 +44,15 @@ driver.get(url)
 driver.implicitly_wait(10)
 
 html = driver.page_source
-
 save_crawler_result(html)
 
-driver.quit()
-
-sys.exit()
-
 tree = etree.HTML(html)
-# links = tree.xpath('//*[@id="SearchMain"]/div/div/div/div/div[position()>2]//a[@class="Link--primary"]/@href')
-# links = tree.xpath('//*[@id="SearchMain"]/div/div/div/div/div[3]/div/div/div/h2/span/div/a/@href')
 links = tree.xpath('//*[@id="SearchMain"]')
 
 print(links)
 
-# etree.tostring(ex).decode()
-
 for link in links:
-    print(link)
+    print(etree.tostring(link).decode())
 
 driver.quit()
 
